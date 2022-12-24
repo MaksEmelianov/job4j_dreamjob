1805a2fa035fc6a6b7494c3db0c954c4efd13c64
Maks Emelianov <mr.maksimemelyanov@ya.ru>
2022-12-24 07:39:01 +0500

    3. VacancyController.create(). Создание вакансии [#504849 #373970]

--- src/main/java/dreamjob/controller/VacancyController.java
+++ src/main/java/dreamjob/controller/VacancyController.java
@@ -1,9 +1,12 @@
 package dreamjob.controller;
 
+import dreamjob.model.Vacancy;
 import dreamjob.store.VacancyRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -20,5 +23,16 @@ public class VacancyController {
     public String getAll(Model model) {
         model.addAttribute("vacancies", vacancyRepository.findAll());
         return "vacancies/list";
+    }
+
+    @GetMapping("/create")
+    public String createGet() {
+        return "vacancies/create";
+    }
+
+    @PostMapping("/create")
+    public String createPost(@ModelAttribute Vacancy vacancy) {
+        vacancyRepository.save(vacancy);
+        return "redirect:/vacancies";
     }
 }
